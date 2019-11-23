72e4b4242887a2f618e1fbaea9e49f883db40933
profileEnumerable <fustethernet@gmail.com>
2019-11-23 23:24:53 +0200

    prefab for main characted was added

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CharState
+{
+    Idle,
+    Run,
+    Jump
+}
+
 public class Character : MonoBehaviour
 {
     [SerializeField] private float speed = 3.0F;
@@ -11,6 +18,12 @@ public class Character : MonoBehaviour
     [SerializeField] private float jumpForce = 17.0F;
 
     private bool isGrounded = false;
+
+    private CharState State
+    {
+        get { return (CharState)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value); }
+    }
 
 
     new private Rigidbody2D rigidbody;
@@ -36,26 +49,35 @@ public class Character : MonoBehaviour
 
     private void Update()
     {
+        if (isGrounded) State = CharState.Idle;
+
         if (Input.GetButton("Horizontal")) Run();
         if (isGrounded && Input.GetButtonDown("Jump")) Jump();
     }
 
     private void Run()
     {
+        if (isGrounded) State = CharState.Run;
+
         Vector3 direction = transform.right * Input.GetAxis("Horizontal");
 
         transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
 
         sprite.flipX = direction.x < 0.0F;
+
     }
 
     private void Jump()
     {
+        State = CharState.Jump;
+
         rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
     }
 
     private void CheckGround()
     {
+        if (!isGrounded) State = CharState.Jump;
+
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3F);
 
         isGrounded = colliders.Length > 1;
